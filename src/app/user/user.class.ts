4c15aed055377534c840812461a4c205e8099105
Hung Dinh <dinhhcoding@gmail.com>
2020-11-16 10:13:13 -0500

    Added user class and service

---
+++ src/app/user/user.class.ts
@@ -0,0 +1,16 @@
+export class User {
+    id: number = 0;
+    userName: string = "";
+    passWord: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    phoneNumber: string = "";
+    email: string = "";
+    reviewer: boolean = false;
+    admin: boolean = false;
+
+    constructor() {
+
+    }
+
+}
