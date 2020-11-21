8bcbd465efadcb24ff954296d5df98d3aab4a132
Hung Dinh <dinhhcoding@gmail.com>
2020-11-21 18:23:44 -0500

    added approve & reject button to request-review-item

--- src/app/request/request-review-item/request-review-item.component.ts
+++ src/app/request/request-review-item/request-review-item.component.ts
@@ -5,6 +5,7 @@ import { Request } from '../request.class';
 import { RequestService } from '../request.service';
 import { LineItem } from '../line-item.class';
 import { LineItemService } from '../line-item.service';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-request-review-item',
@@ -41,5 +42,26 @@ export class RequestReviewItemComponent implements OnInit {
       },
       err => { console.error(err);}
     );
+  }
+
+  approveRequest(): void {
+    this.requestsvc.approve(this.request).subscribe(
+      res => {
+        console.debug("Request Approved:", res);
+        this.router.navigateByUrl("/requests/review")
+      },
+    err => { console.error("Error approving request:", err);}
+    )
+  }
+
+
+  rejectRequest(): void {
+    this.requestsvc.reject(this.request).subscribe(
+      res => {
+        console.debug("Request Rejected:", res);
+        this.router.navigateByUrl("/request/review")
+      },
+      err => { console.error("Error rejecing request:", err);}
+    )
   }
 }
