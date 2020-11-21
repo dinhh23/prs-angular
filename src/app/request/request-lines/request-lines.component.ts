afc48f30ef40a5372c3d36707d989fd61f60d0f3
Hung Dinh <dinhhcoding@gmail.com>
2020-11-20 19:32:54 -0500

    added another refresh method to refresh the request on the page

--- src/app/request/request-lines/request-lines.component.ts
+++ src/app/request/request-lines/request-lines.component.ts
@@ -53,21 +53,33 @@ export class RequestLinesComponent implements OnInit {
     )
  }
 
-
-refresh(): void {
+// refresh the request detail on the page without routing to a different page
+refreshR(): void {
  let id =this.route.snapshot.params.id;
   this.requestsvc.get(id).subscribe(
-   res => {console.debug("Refresh works:", res);
+   res => {console.debug("Refresh request works:", res);
    this.request = res;
   },
-  err => {console.error("Refresh doesn't work:", err);}
+  err => {console.error("Refresh request doesn't work:", err);}
+ );
+ }
+
+ // refresh the line items detail on the page without routing to a different page
+refreshLI(): void {
+ let id =this.route.snapshot.params.id;
+  this.lineitemssvc.linesForReq(id).subscribe(
+   res => {console.debug("Refresh line items works:", res);
+   this.lineitems = res;
+  },
+  err => {console.error("Refresh line items doesn't work:", err);}
  );
  }
 
 delete(lineitem: LineItem): void {
   this.lineitemssvc.remove(lineitem).subscribe(
     res => {console.debug("Lineitem removed:", res);
-    this.refresh();
+    this.refreshR();
+    this.refreshLI(); 
   },
     err => {console.error("Error deleting lineitem", err);}
   );
